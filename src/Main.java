db73585d91cfdc0b19c7e7fb66cf83b408cefdef
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-08-04 18:15:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main{
-   public static void main(String[] args) {
-        /*add your code here*/
-   }
-}
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7);
+        list.append(8);
+        list.append(9);
+        list.append(10);
 
+        list.linearSearch(9);
+    }
+}
