23ec6491cd1225b34b80dd82898f9814c9e7709d
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-08-04 18:15:39 +0300

    Update DoublyLinkedList.java

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -31,20 +31,3 @@ class DoublyLinkedList {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        DoublyLinkedList list = new DoublyLinkedList();
-        list.append(1);
-        list.append(2);
-        list.append(3);
-        list.append(4);
-        list.append(5);
-        list.append(6);
-        list.append(7);
-        list.append(8);
-        list.append(9);
-        list.append(10);
-
-        list.linearSearch(9);
-    }
-}
